6ed43b429bc9254c108995dee7925bac65ecef35
Unknown <Unknown@DESKTOP-KO6EVLM>
2025-01-26 18:52:23 +0800

    Initial code

---
+++ SignalRWinform.Client/MainForm.cs
@@ -0,0 +1,10 @@
+namespace SignalRWinform.Client
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ SignalRWinform.Server/MainForm.cs
@@ -0,0 +1,10 @@
+namespace SignalRWinform.Server
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
